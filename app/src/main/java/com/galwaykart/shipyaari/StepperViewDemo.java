8ded4b1720ba0144f63c3adec08d3e9c29d1780f
Ankesh kumar <ankesh.kumar@sakshemit.com>
2020-03-18 14:43:27 +0530

    address changes

--- app/src/main/java/com/galwaykart/shipyaari/StepperViewDemo.java
+++ app/src/main/java/com/galwaykart/shipyaari/StepperViewDemo.java
@@ -112,8 +112,8 @@ public class StepperViewDemo extends BaseActivity {
         st_selected_shipping_type = pref.getString("st_selected_shipping_type","");
 
 
-        st_selected_Track_id="20135663354";
-        st_selected_shipping_type="2";
+        st_selected_Track_id="3587910267960";
+        st_selected_shipping_type="1";
 
 
         //Log.d("st_selected_Track_id", st_selected_Track_id);
@@ -221,7 +221,6 @@ public class StepperViewDemo extends BaseActivity {
                  intent.putExtra("trackUrl", "https://www.fedex.com/apps/fedextrack/?action=track&trackingnumber=" + st_selected_Track_id);
                  startActivity(intent);
              }
-
 
 
 
@@ -520,8 +519,8 @@ st_request_URL=Global_Settings.st_sales_api+"DTDCTrackShip/"+st_selected_Track_i
     String input_data="";
     private void trackShipment() {
 
-         st_request_URL = Global_Settings.shipyaari_api_url;
-         input_data="{\"trackingnumber\":\"" + st_selected_Track_id + "\"}";
+         st_request_URL = Global_Settings.shipyaari_api_url+"st_selected_Track_id";
+         //input_data="{\"trackingnumber\":\"" + st_selected_Track_id + "\"}";
 
         pDialog = new TransparentProgressDialog(StepperViewDemo.this);
         pDialog.setCancelable(false);
@@ -533,7 +532,7 @@ st_request_URL=Global_Settings.st_sales_api+"DTDCTrackShip/"+st_selected_Track_i
             final RequestQueue requestQueue = Volley.newRequestQueue(this);
 
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, st_request_URL,
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, st_request_URL,
                     new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
@@ -543,6 +542,9 @@ st_request_URL=Global_Settings.st_sales_api+"DTDCTrackShip/"+st_selected_Track_i
                             try {
 
                                 JSONObject object = new JSONObject(String.valueOf(response));
+
+
+                                JSONObject jsonObject=new JSONObject("lifecycle");
 
                                 st_trackingnumber = object.getString("trackingnumber");
                                 st_partnerID = object.getString("partnerID");
@@ -616,10 +618,10 @@ st_request_URL=Global_Settings.st_sales_api+"DTDCTrackShip/"+st_selected_Track_i
                     return "utf-8";
                 }
 
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    return input_data == null ? null : input_data.getBytes(StandardCharsets.UTF_8);
-                }
+//                @Override
+//                public byte[] getBody() throws AuthFailureError {
+//                    return input_data == null ? null : input_data.getBytes(StandardCharsets.UTF_8);
+//                }
 
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
