8f9c6601efdd0399668df0aaea05ed55508d7a0b
ankesh-kumar <mail.ankeshkumar@gmail.com>
2020-08-07 17:37:13 +0530

    4.1.0 Release

--- app/src/main/java/com/galwaykart/essentialClass/Global_Settings.java
+++ app/src/main/java/com/galwaykart/essentialClass/Global_Settings.java
@@ -14,7 +14,7 @@ public class Global_Settings {
     System.loadLibrary("commonfun");
  }
 
-    public static final int current_soft_version=93;
+    public static final int current_soft_version=94;
     // public static final String test_offer_url="http://qa.galwaykart.com/24nov/galway_sus/";
     //public static final String Notification_api_url="http://test.glazegalway.com:81/ios_galwaykart/";
     //public static final String galway_api_url=commonSalesData();
@@ -35,9 +35,9 @@ public class Global_Settings {
     public static final String st_sales_api=galway_api_url+"returnapi/";     // Live
     //public static final String st_sales_api="http://it.galway.in/returnapi/";// Test
 
-    //public static final String api_url="https://www.galwaykart.com/";                 // Live
+    public static final String api_url="https://www.galwaykart.com/";                 // Live
     //public static final String api_url="http://192.168.10.130/galwaykart/";               // Test
-    public static final String api_url="http://qa.galwaykart.com/";      // new qa server
+    //public static final String api_url="http://qa.galwaykart.com/";      // new qa server
 
 
     //public static final String online_payment_hash_key_url="https://www.galwaykart.com/";
